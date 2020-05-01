8e5f1a46acdf559d39ae171f8407755444cc00ff
M <magic.ru789@gmail.com>
2020-04-30 22:16:16 -0500

    delete client pointer on exit

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@ int main(int argc, char* argv[]) {
     // Create and start the xrouter client
     auto client = std::unique_ptr<xrouter::XRouterClient>(new xrouter::XRouterClient(xrouter::XRouterClient::defaultOptions()));
     if (!client->start()) {
-        std::cout << "Failed to find service" << std::endl;
+        std::cout << "Failed to start client" << std::endl;
+        client = nullptr;
         std::exit(EXIT_FAILURE);
     }
     // Wait up to 10 seconds for the service to be found
     if (!client->waitForService(10000, "xr::BLOCK")) {
         std::cout << "Failed to find service" << std::endl;
         client->stop();
+        client = nullptr;
         std::exit(EXIT_FAILURE);
     }
     // Get the block count for the xr::BLOCK SPV service
